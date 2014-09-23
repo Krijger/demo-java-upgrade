13dbef843de913561b7b77ed02f2b414f8b22950
Jaroslav Holub <jholub@gmail.com>
2014-09-23 09:14:34 +0200

    Updated ui to disable buttons during the build process. Added host stdout to the logs.

--- frontend/hot-swap-demo.js
+++ frontend/hot-swap-demo.js
@@ -6,6 +6,18 @@ DockerfilePath = BasePath + '/Dockerfile';
 Logs = new Mongo.Collection("logs");
 
 if (Meteor.isClient) {
+    Deps.autorun(function() {
+        Meteor.ClientCall.setClientId('demo');
+    });
+    Meteor.ClientCall.methods({
+        'enableInterface': function() {
+            $('button').removeClass('disabled');
+        },
+        'disableInterface': function() {
+            $('button').addClass('disabled');
+        }
+    });
+    
     // Logs
     Meteor.subscribe('logs');
     Template.logs.entries = function() {
@@ -47,16 +59,43 @@ if (Meteor.isServer) {
     var fs = Npm.require('fs');
     var process = Npm.require('child_process');
     var spawn = Npm.require('child_process').spawn;
+
+    var addEntry = function(data) {
+        data.split(/\r?\n/).forEach(function(entry) {
+            entry && Logs.insert({entry: entry});
+        });
+    };
+
+    var tailDockerLogs = function() {
+        var tail = spawn('docker', ['logs', '-f', ContainerName]);
+        tail.stdout.setEncoding('utf8');
+        tail.stdout.on('data', Meteor.bindEnvironment(addEntry));
+        tail.stdout.on('end', Meteor.bindEnvironment(function() {
+            setTimeout(Meteor.bindEnvironment(function() {
+                tailDockerLogs();
+            }), 300);
+        }));
+    };
+
     Meteor.methods({
         updateDockerfile: function(content) {
+            Meteor.ClientCall.apply('demo', 'disableInterface');
             fs.writeFile(DockerfilePath, content);
-            process.exec('cd ' + BasePath + ' && ./build.sh', {maxBuffer: 1024 * 500}, function (error, stdout, stderr) {
-                if (error) {
-                    console.log(error.stack);
-                    console.log('Error code: '+error.code);
-                    console.log('Signal received: '+error.signal);
-                }
-            });
+            process.exec(
+                'cd ' + BasePath + ' && ./build.sh',
+                {maxBuffer: 1024 * 500},
+                Meteor.bindEnvironment(function (error, stdout, stderr) {
+                    addEntry(stdout);
+                    var appLoadingTimeout = Meteor.setInterval(function() {
+                        try {
+                            if (Meteor.http.get(ContainerURL).content) {
+                                Meteor.ClientCall.apply('demo', 'enableInterface');
+                                Meteor.clearInterval(appLoadingTimeout);
+                            }
+                        } catch (exception) {}
+                    }, 500);
+                })
+            );
         },
         readDockerfile: function() {
             return fs.readFileSync(DockerfilePath, 'utf-8');
@@ -69,23 +108,9 @@ if (Meteor.isServer) {
 
     Meteor.startup(function() {
         Logs.remove({});
-        var addEntry = function(data) {
-            data.split(/\r?\n/).forEach(function(entry) {
-                entry && Logs.insert({entry: entry});
-            });
-        };
-        var tailDockerLogs = function() {
-            var tail = spawn('docker', ['logs', '-f', ContainerName]);
-            tail.stdout.setEncoding('utf8');
-            tail.stdout.on('data', Meteor.bindEnvironment(addEntry));
-            tail.stdout.on('end', Meteor.bindEnvironment(function() {
-                setTimeout(Meteor.bindEnvironment(function() {
-                    tailDockerLogs();
-                }), 300);
-            }));
-        }
         tailDockerLogs();
     });
+
     Meteor.publish('logs', function() {
         return Logs.find({});
     });
